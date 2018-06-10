b8ba72d8071d3c635c7827cacc10a1ea24610364
Arjen <alphabravoab@gmail.com>
2018-06-10 10:32:26 +0200

    callback working promise failing

--- src/async-functions.js
+++ src/async-functions.js
@@ -1,16 +1,29 @@
-function giveItBackLater(){
-
+function giveItBackLater(value, callback){
+  function loadComplete() {
+      callback(value)
+    }
+  setTimeout(loadComplete, 100)
 }
 
-function addSomePromises(){
+function addSomePromises(promisesPromises){
+  return new Promise((resolve, reject) => {
+    function loadComplete() {
+      if (test === undefined) {
+        reject(promisesPromises.concat(promisesPromises.concat(promisesPromises)))
+      }
 
+      resolve(promisesPromises.concat(promisesPromises))
+    }
+    setTimeout(loadComplete, 1000)
+  })
 }
+
 
 function promiseToGiveItBackLater(){
 
 }
 
-  
+
 
 
 
