eba232d9ea1bd236b0a96a49813030ca374c7c04
Arjen <alphabravoab@gmail.com>
2018-06-10 17:18:07 +0200

    got the class require to work

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -2,16 +2,22 @@ class ShoppingCart{
   constructor(items){
     this.items=[]
   }
-  addItem(){
+  getItems(){
+    return this.items
+  }
+  addItem(itemName, quantity, price ){
     return this.items.push({'name': itemName, 'quantity': quantity, 'pricePerUnit': price})
   }
   clear(){
     return this.items.length=0
   }
+  clone(){
+    return new ShoppingCart(this.items.map((item) => Object.assign({}, item )) )
+  }
 }
 
 
 
-console.log(cart = new ShoppingCart());
+console.log(cart = new ShoppingCart())
 
-module.exports = { ShoppingCart }
+module.exports =  ShoppingCart
