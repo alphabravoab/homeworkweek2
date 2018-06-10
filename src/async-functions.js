7e9772ea9b2eef89f66c07b506c58a4e986e95d7
Arjen <alphabravoab@gmail.com>
2018-06-10 10:10:41 +0200

    all files exist second assignment mostly working

---
+++ src/async-functions.js
@@ -0,0 +1,19 @@
+function giveItBackLater(){
+
+}
+
+function addSomePromises(){
+
+}
+
+function promiseToGiveItBackLater(){
+
+}
+
+  
+
+
+
+
+
+module.exports = { giveItBackLater, addSomePromises, promiseToGiveItBackLater }
--- src/pathFind.js
+++ src/pathFind.js
@@ -1,29 +1,9 @@
 function pathFind(path, object){
-//console.log(object.foo1.foo2)
   const t=object
-//  console.log(path[path.length-1]);
-    test = path.reduce((acc, val,currentIndex)=>{
-    //  console.log("ACC ", typeof(acc))
-    //  if(t[path[currentIndex]])===undefined){pathFind(val,acc)}
-    //  console.log("CI ", currentIndex);
-    //  console.log("object item", t[path[currentIndex]]);
-      if(typeof(t[path[currentIndex]])===object){pathFind(val, acc)}
-     return acc, val
-//  if (typeof (namer)== object)
-
-
-})
-//other idea: if statement. if it is a string return it if not run function.
-  return t[path[0]]
+  if(TypeError(object[path[0]])==object){
+    pathFind(path.shift(), object[path[0]])
+  }else{
+    return t[path[0]]
+  }
 }
 module.exports = { pathFind }
-
-const path = ["foo1", "foo2", "bar"]
-const obj = {
-    foo1: {
-        foo2: {
-            bar: "Some value"
-        }
-    }
-  }
-  pathFind(path, obj)
